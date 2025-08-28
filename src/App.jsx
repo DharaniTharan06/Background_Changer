41223a2d7b24602f4796c71ae371f1d8a2ba5876
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-29 00:27:13 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [color,setcolor] = useState("white")
 
   return (
-    <div className='fixed w-full h-screen inset-0' style={{backgroundColor: color}}>
+    <div className='w-full h-screen inset-0' style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
           <button onClick={()=>{setcolor("red")}}className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: "red"}}>Red</button>
